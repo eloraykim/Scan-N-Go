7907b8f849e549303c9a2b56cc3f91142099d23e
Elora Kim <eloraykim@gmail.com>
2020-07-14 10:38:38 -0700

    Initial commit

---
+++ Scan N Go/ToDo.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo.swift
+//  Scan N Go
+//
+//  Created by Elora Kim on 7/13/20.
+//  Copyright © 2020 Elora Kim. All rights reserved.
+//
+
+import UIKit
+
+class ToDoList {
+    var name = ""
+    var important = false
+}
+
+var newToDoList = ToDoList ()
+print(newToDoList)
